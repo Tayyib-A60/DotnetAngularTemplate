6931a8c3e0c83435fdd7d2a743d9887a46dc4f7f
tayyib-a60 <adesokantayyib@gmail.com>
2019-06-18 15:07:35 +0100

    initial commit

---
+++ API/Mapping/MappingProfile.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace API.Mapping
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            // CreateMap<Source, Destination>();
+        }
+    }
+}
